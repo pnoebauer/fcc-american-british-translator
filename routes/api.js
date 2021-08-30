f8a8c55e400f27811d93b028c4f79b438bcb4938
Philip Noebauer <pnoebauer>
2021-08-30 08:37:52 +0800

    first commit

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Translator = require('../components/translator.js');
+
+module.exports = function (app) {
+	const translator = new Translator();
+
+	app.route('/api/translate').post((req, res) => {
+		console.log(req.body);
+		const {text, locale} = req.body;
+	});
+};
