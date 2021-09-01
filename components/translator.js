244c4e151eed2323725a4245641a1f53f672ed08
Philip Noebauer <pnoebauer>
2021-09-01 09:37:49 +0800

    work on translating time formats

--- components/translator.js
+++ components/translator.js
@@ -4,6 +4,26 @@ const americanToBritishTitles = require('./american-to-british-titles.js');
 const britishOnly = require('./british-only.js');
 
 class Translator {
+	translateTime(text, locale) {
+		const usedTimeSeparator = locale === 'american-to-british' ? ':' : '.';
+		const newTimeSeparator = locale === 'american-to-british' ? '.' : ':';
+
+		const hr24OptLeadZero = `([0-9]|0[0-9]|1[0-9]|2[0-3])${usedTimeSeparator}[0-5][0-9]`;
+		const times = text.match(new RegExp(hr24OptLeadZero, 'g'));
+		console.log(hr24OptLeadZero, times);
+
+		if (!times) {
+			return text;
+		}
+
+		times.forEach(time => {
+			const convertedTime = time.replace(usedTimeSeparator, newTimeSeparator);
+			text = text.replace(time, convertedTime);
+		});
+
+		return text;
+	}
+
 	americanToBritish(text) {
 		const americanToBritishMap = {
 			...americanOnly,
@@ -15,7 +35,27 @@ class Translator {
 			const britishWord = americanToBritishMap[americanWord];
 			text = text.replace(new RegExp(`\\b${americanWord}\\b`, 'g'), britishWord);
 		});
+		// console.log(text);
+
+		// // const hr24OptLeadZero = /([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/g;
+		// // console.log(text.match(/(10|11|12|[1-9]):[0-5][0-9]/g));
+		// // console.log(text.match(hr24OptLeadZero));
+		// // const times = text.match(hr24OptLeadZero);
+
+		// // const hr24OptLeadZero = /([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/g;
+		// const hr24OptLeadZero = `([0-9]|0[0-9]|1[0-9]|2[0-3])${':'}[0-5][0-9]`;
+		// const times = text.match(new RegExp(hr24OptLeadZero, 'g'));
+
+		// times.forEach(americanTime => {
+		// 	const britishTime = americanTime.replace(':', '.');
+		// 	text = text.replace(americanTime, britishTime);
+		// });
+
+		text = this.translateTime(text, 'american-to-british');
+
 		console.log(text);
+
+		return text;
 	}
 
 	britishToAmerican(text) {
@@ -32,7 +72,11 @@ class Translator {
 			const britishWord = americanToBritishMap[americanWord];
 			text = text.replace(new RegExp(`\\b${britishWord}\\b`, 'g'), americanWord);
 		});
-		console.log(text);
+		// console.log(text);
+
+		text = this.translateTime(text, 'british-to-american');
+
+		return text;
 	}
 }
 
--- routes/api.js
+++ routes/api.js
@@ -8,5 +8,15 @@ module.exports = function (app) {
 	app.route('/api/translate').post((req, res) => {
 		console.log(req.body);
 		const {text, locale} = req.body;
+
+		if (locale === 'american-to-british') {
+			const translation = translator.americanToBritish(text);
+
+			return res.json({text, translation});
+		} else if (locale === 'british-to-american') {
+			const translation = translator.britishToAmerican(text);
+
+			return res.json({text, translation});
+		}
 	});
 };
